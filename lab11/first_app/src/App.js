d3ccd1f1a4967feccee137b78acbe1fc614c09fb
Jxsephhhh <joseph.bernabe89@login.cuny.edu>
2025-04-01 19:36:31 +0000

    start lab 11

---
+++ lab11/first_app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Joseph Bernabe</h1>
+      <p>This is my first React.JS App</p>
+    </div>
+  );
+}
+
+export default App;
